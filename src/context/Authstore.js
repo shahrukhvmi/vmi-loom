ec78d6c29a4009ef9ad81de836e5544764675046
usmanwarind46 <usmanwarind.vmi@gmail.com>
2026-07-10 18:36:12 +0500

    change

--- src/context/Authstore.js
+++ src/context/Authstore.js
@@ -1,8 +1,17 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useAuthStore = create((set) => ({
-  user: null,
-  token: null,
-  setAuth: (user, token) => set({ user, token }),
-  clearAuth: () => set({ user: null, token: null }),
-}));
+export const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      setAuth: (user, token) => set({ user, token }),
+      clearAuth: () => set({ user: null, token: null }),
+    }),
+    {
+      name: "recrd-auth", // localStorage key
+      partialize: (s) => ({ user: s.user, token: s.token }), // only persist these
+    },
+  ),
+);
